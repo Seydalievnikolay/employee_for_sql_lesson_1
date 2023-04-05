490c318c75797832ae92c5ad7c91f657619476f3
Seydalievnikolay <nikolay-seydalie@yandex.ru>
2023-04-05 21:22:04 +0700

    second lesson

--- employee.sql
+++ employee.sql
@@ -8,8 +8,13 @@ CREATE DATABASE skypro;
             age INT NOT NULL
         );
 INSERT INTO employee (
-        VALUES ('Julia', 'Petrova', 'W', 19)
-        VALUES ('Semen', 'Ivanov', 'M', 30)
+    first_name, last_name, gender, age)
+        VALUES ('Julia', 'Petrova', 'W', 19);
+INSERT INTO employee (
+    first_name, last_name, gender, age)
+        VALUES ('Semen', 'Ivanov', 'M', 30);
+INSERT INTO employee (
+    first_name, last_name, gender, age)
         VALUES ('Pavel', 'Sidorov', 'M', 20);
 SELECT*FROM employee;
 UPDATE employee SET first_name = 'Evgeniy' WHERE id = 1;
@@ -19,3 +24,12 @@ UPDATE employee SET age = 18 WHERE id = 1;
 SELECT*FROM employee;
 DELETE FROM employee WHERE id = 1;
 SELECT*FROM employee;
+INSERT INTO employee (
+    first_name, last_name, gender, age)
+        VALUES ('Grigoriy', 'Semenov', 'M', 32);
+INSERT INTO employee (
+    first_name, last_name, gender, age)
+        VALUES ('Uliyana', 'Ivantcova', 'W', 24);
+INSERT INTO employee (
+    first_name, last_name, gender, age)
+        VALUES ('Mikhail', 'Udaltcov', 'M', 110);
