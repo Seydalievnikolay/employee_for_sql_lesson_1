43ad56a0e0df87d2c72ca409c8ffd1ed0c9b5df4
Seydalievnikolay <nikolay-seydalie@yandex.ru>
2023-04-08 18:09:01 +0700

    first lesson( Data sampling and nested queries )

--- employee.sql
+++ employee.sql
@@ -33,3 +33,26 @@ INSERT INTO employee (
 INSERT INTO employee (
     first_name, last_name, gender, age)
         VALUES ('Mikhail', 'Udaltcov', 'M', 110);
+SELECT first_name AS Имя, last_name AS Фамилия
+FROM employee;
+SELECT * FROM employee WHERE age<30 OR age>50;
+SELECT * FROM employee WHERE age BETWEEN 30 AND 50;
+SELECT *FROM employee ORDER BY last_name DESC;
+SELECT * FROM employee WHERE LENGTH (first_name) > 4;
+--вторая задача
+SELECT * FROM employee;
+UPDATE employee SET first_name = 'Semen' WHERE id = 4;
+UPDATE employee SET first_name = 'Evgeniya' WHERE id = 5;
+SELECT first_name AS Имя,
+SUM(age) AS Суммарный_возраст
+FROM employee
+GROUP BY Имя;
+SELECT first_name,
+age FROM employee
+WHERE age = (SELECT MIN (age)
+FROM employee);
+SELECT first_name AS Имя,
+MAX(age) AS Максимальный_возраст
+FROM employee
+GROUP BY first_name
+HAVING COUNT(first_name)>1;
